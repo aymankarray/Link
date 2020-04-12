e08473b02e9a2c4193d3579e7f9d98a15f39ae5d
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-04-12 22:28:24 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -6,7 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Tunivisions Link </title>
       <!-- Favicon -->
-      <link rel="shortcut icon" href="<?php echo base_url() ;  ?>images/favicon.ico">
+      <link rel="shortcut icon" href="<?php echo base_url() ;  ?>images/icon.svg">
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="<?php echo base_url() ;  ?>assets/css/bootstrap.min.css">
       <!-- Typography CSS -->
