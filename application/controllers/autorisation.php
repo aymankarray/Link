dc9f03400b79e1e1cf90de55ac4d467ce7aca448
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-04-20 16:53:37 +0100

    Update autorisation.php

--- application/controllers/autorisation.php
+++ application/controllers/autorisation.php
@@ -9,7 +9,7 @@ require APPPATH . '/libraries/BaseController.php';
  * @version : 1.1
  * @since : 15 November 2016
  */
-class Autorisation extends BaseController
+class Autorisation extends  CI_Controller
 {
     /**
      * This is default constructor of the class
