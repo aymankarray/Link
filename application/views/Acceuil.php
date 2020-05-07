f8b5164dc761a90b49bcc088e6943ee90c2f852c
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-07 15:16:00 +0100

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -209,17 +209,33 @@
                                  </div>
                               </div>
                               <div class="mt-3" >
-                                 <p  id="post<?php echo $record->postId ?>" ><?php echo $record->Content ?></p>
-                                    <script type="text/javascript">
-                                                 function convert()
-                                                 {
-                                                  var text= "<?php echo $record->Content ?>" ;
-                                                  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                                                  var text1=text.replace(exp, "<a href='$1'>$1</a>");
-                                                  var exp2 =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
-                                                  document.getElementById("converted_url").innerHTML=text1.replace(exp2, '$1<a target="_blank" href="http://$2">$2</a>');
-                                                 }
-                                               </script>
+                                 <p  id="post<?php echo $record->postId ?>"  >
+                                    <?php 
+
+                                                                                 // The Regular Expression filter
+                                          $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+
+                                          // The Text you want to filter for urls
+                                          $text = $record->Content ;
+
+                                          // Check if there is a url in the text
+                                          if(preg_match($reg_exUrl, $text, $url)) {
+
+                                                 // make the urls hyper links
+                                                 echo preg_replace($reg_exUrl, "<a href="{$url[0]}">{$url[0]}</a> ", $text);
+
+                                          } else {
+
+                                                 // if no urls in the text just return the text
+                                                 echo $text;
+
+                                          }
+
+                                          if ($reg_exUrl != Null){ }
+
+
+                                    ?></p>
+
                               </div>
                               
                               <div class="comment-area mt-3">
