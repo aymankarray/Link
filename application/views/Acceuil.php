f73f3fdd95aeb05a3d2da1c6f9df334d37591afc
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-07 15:24:26 +0100

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -212,23 +212,12 @@
                                  <p   >
                                     <?php 
 
-                                                                                 // The Regular Expression filter
                                           $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-
-                                          // The Text you want to filter for urls
                                           $text = $record->Content ;
-
-                                          // Check if there is a url in the text
                                           if(preg_match($reg_exUrl, $text, $url)) {
-
-                                                 // make the urls hyper links
                                                  echo preg_replace($reg_exUrl, "<a href="{$url[0]}">{$url[0]}</a> ", $text);
-
                                           } else {
-
-                                                 // if no urls in the text just return the text
                                                  echo $text;
-
                                           }
 
                                     ?>
