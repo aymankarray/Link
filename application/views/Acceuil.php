9cc353ccbeef034a523be98374da19babb5500c7
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-07 15:25:42 +0100

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -215,7 +215,7 @@
                                           $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
                                           $text = $record->Content ;
                                           if(preg_match($reg_exUrl, $text, $url)) {
-                                                 echo preg_replace($reg_exUrl, "<a href="{$url[0]}">{$url[0]}</a> ", $text);
+                                                 echo preg_replace($reg_exUrl, "<a href=".$url[0].">".$url[0]."</a> ", $text);
                                           } else {
                                                  echo $text;
                                           }
