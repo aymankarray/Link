8fc8ad6abbbe8153eebe0098d4515b8d86223f11
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-20 20:48:30 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,11 +7,12 @@
                 
                 <div class="row ">
                         <div class="col-md-4 grid-margin stretch-card">
-                            <div class="card table-dark">
+                            <div class="card">
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0 ">Clubs Senior</h6>
                                         <div class="dropdown mb-2">
+                                            dddd
                                         </div>
                                     </div>
                                     <div class="row">
