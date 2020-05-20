8a742118962f066abd98d9a6838704c04f3fa52f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-20 19:15:58 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -142,7 +142,7 @@
                                         <?php  if(!empty($RateMember)){ foreach ($RateMember as $membre ) { ?>
                                           <tr>
 
-                                            <td  > <img src="https://www.tunivisions.link/uploads/avatar/<?php echo $membre->avatar ?>" class="alligator-turtle  mr-3" alt="user"> </td>
+                                            <td  > <img src="https://www.tunivisions.link/uploads/avatar/<?php echo $membre->avatar ?>" class="alligator-turtle " alt="user"> </td>
 
                                             <td> <small><small>
 
@@ -158,11 +158,14 @@
 
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 7a8 8 0 1 1 0 16 8 8 0 0 1 0-16zm0 3.5l-1.323 2.68-2.957.43 2.14 2.085-.505 2.946L12 17.25l2.645 1.39-.505-2.945 2.14-2.086-2.957-.43L12 10.5zm1-8.501L18 2v3l-1.363 1.138A9.935 9.935 0 0 0 13 5.049L13 2zm-2 0v3.05a9.935 9.935 0 0 0-3.636 1.088L6 5V2l5-.001z" fill="rgba(155,77,9,1)"/></svg>
 
-                                            <?php   }  ?><?php echo $membre->name ?> <br>
+                                            <?php   }  ?>
+                                            <div>
+                                            <?php echo $membre->name ?> <br>
                                             <small><small><?php echo $membre->clubName ?></small></small>
-                                            
+                                            </div>
 
                                             </small></small> </td>
+
                                             <td> <p class="text-muted "><small><small><?php echo $membre->scores ?> Points</small></p> </td>
                                           </tr>
                                           
