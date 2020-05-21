b06349cb1a88b4ea3a9e7cbcc3485e34f3edf99e
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-21 16:09:53 +0100

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -1,16 +1,4 @@
-<?php 
-                     function xTimeAgo ($oldTime, $newTime) {
-                      $timeCalc = strtotime($newTime) - strtotime($oldTime);
 
-                      if ($timeCalc > (60*60*24)) {$timeCalc = round($timeCalc/60/60/24) . " jours";}
-                      else if ($timeCalc > (60*60)) {$timeCalc = round($timeCalc/60/60) . " heures";}
-                      else if ($timeCalc > 60) {$timeCalc = round($timeCalc/60) . " minutes";}
-                      else if ($timeCalc > 0) {$timeCalc .= " seconds";}
-
-                      return $timeCalc;
-                      }
-
-                      ?>
 <style type="text/css">
    #upload{
     display:none
