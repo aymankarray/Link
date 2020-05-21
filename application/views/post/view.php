5b27ae42abb6a153832f7e8be3752c564e3bd5d6
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-21 19:24:51 +0100

    Update view.php

--- application/views/post/view.php
+++ application/views/post/view.php
@@ -157,7 +157,7 @@
                                              <img class="alligator-turtle  mr-3" src="https://tunivisions.link/uploads/avatar/<?php echo $key->avatar ?>"  >
                                           </div>
                                           <div class="comment-data-block ml-3">
-                                             <h5><a href="https://tunivisions.link/User/ProfileShow/<?php echo $key->comUserId ?>"  > <?php echo $key->name ; ?> </a></h5>
+                                             <h5><a href="https://tunivisions.link/User/ProfileShow/<?php echo $key->comUserId ?>" class="text-primary"  > <?php echo $key->name ; ?> </a></h5>
                                             
                                              <p class="mb-0"><?php echo $key->content ; ?></p>
                                              <div class="d-flex flex-wrap align-items-center comment-activity">
@@ -169,16 +169,17 @@
                                     </li>
                                     <?php  } }    ?>                                 
                                  </ul>
-                                 <!--
+                                
                                  <form class="comment-text d-flex align-items-center mt-3" action="javascript:void(0);">
                                     <input type="text" class="form-control rounded">
                                     <div class="comment-attagement d-flex">
-                                       <a href="javascript:void();"><i class="ri-link mr-3"></i></a>
+                                      <!-- <a href="javascript:void();"><i class="ri-link mr-3"></i></a>
                                        <a href="javascript:void();"><i class="ri-user-smile-line mr-3"></i></a>
                                        <a href="javascript:void();"><i class="ri-camera-line mr-3"></i></a>
+                                       -->
                                     </div>
                                  </form>
-                              -->
+                            
                               </div>
                            </div>
                         </div>
