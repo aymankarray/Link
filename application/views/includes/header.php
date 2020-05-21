999270e2eafa947410904748810479b61f8dec99
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-21 13:36:20 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -883,10 +883,10 @@
                      function xTimeAgo ($oldTime, $newTime) {
                       $timeCalc = strtotime($newTime) - strtotime($oldTime);
 
-                      if ($timeCalc > (60*60*24)) {$timeCalc = round($timeCalc/60/60/24) . ” days ago”;}
-                      else if ($timeCalc > (60*60)) {$timeCalc = round($timeCalc/60/60) . ” hours ago”;}
-                      else if ($timeCalc > 60) {$timeCalc = round($timeCalc/60) . ” minutes ago”;}
-                      else if ($timeCalc > 0) {$timeCalc .= ” seconds ago”;}
+                      if ($timeCalc > (60*60*24)) {$timeCalc = round($timeCalc/60/60/24) . " jours";}
+                      else if ($timeCalc > (60*60)) {$timeCalc = round($timeCalc/60/60) . " heures";}
+                      else if ($timeCalc > 60) {$timeCalc = round($timeCalc/60) . " minutes";}
+                      else if ($timeCalc > 0) {$timeCalc .= " seconds";}
 
                       return $timeCalc;
                       }
