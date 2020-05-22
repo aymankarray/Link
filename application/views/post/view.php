843c1f7516c9ae16e955a1fbb0cc097778ccae91
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-22 14:59:54 +0100

    Update view.php

--- application/views/post/view.php
+++ application/views/post/view.php
@@ -116,7 +116,7 @@
                                           <div class="total-like-block ml-2 mr-3">
                                              
                                                
-                                          <a href="#"  id="toLike<?php echo $postRecords->postId ?>" onclick="like(this.id)" > j'aime </a>  
+                                          <a   id="toLike<?php echo $postRecords->postId ?>" onclick="like(this.id)" > j'aime </a>  
                                                    
                                                 
                                             
