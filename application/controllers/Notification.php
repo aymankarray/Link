36b135fef7e48fa14bd1c5802cdd8360aa70eeb1
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-22 19:25:42 +0100

    Update Notification.php

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -1,5 +1,6 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
+require APPPATH . '/libraries/BaseController.php';
 /**
  * Class : Login (LoginController)
  * Login class to control to authenticate user credentials and starts user's session.
