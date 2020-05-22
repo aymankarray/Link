20e03840905ccede62955266bdfd958c7266e90f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-22 17:29:56 +0100

    user deletehis comment

--- application/views/post/view.php
+++ application/views/post/view.php
@@ -153,7 +153,7 @@
 
                                                  </span>
                                                  .
-                                                 <?php if ($userID == $key->userId ){  ?>
+                                                 <?php if ($userId == $key->userId ){  ?>
                                                  <a class="text"  id="<?php echo $key->commentId ?>" onclick="deleteComment(this.id)" > supprimer </a>
                                                  <?php } ?>
                                              </div>
