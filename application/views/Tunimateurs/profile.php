502123b9850e5cb60d2acfc47d7f2515702656af
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-23 00:40:13 +0100

    Update profile.php

--- application/views/Tunimateurs/profile.php
+++ application/views/Tunimateurs/profile.php
@@ -320,19 +320,22 @@
             </div>
             <div class="mt-3">
                 <p style="font-size: 13;">
-                    <?php 
+                   <?php 
 
-                  $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-                  $text = $record->Content ; 
-                  if(preg_match($reg_exUrl, $text, $url)) {
-                     
-                     echo preg_replace($reg_exUrl, "<a href=".$url[0]." target="_blank">".$url[0]."</a> ", $text);
-                      } 
-                  else
-                      { echo $text; } 
-                   if ($reg_exUrl = "(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*" ) { ?>
+                                          $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+                                          $text = $record->Content ;
+                                          if(preg_match($reg_exUrl, $text, $url)) {
+                                                 echo preg_replace($reg_exUrl, "<a href=".$url[0]." target=_blank >".$url[0]."</a> ", $text);
+                                          } else {
+                                                 echo $text;
+                                          }
 
-                    <?php
+                                          if ($reg_exUrl = "(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*" ) { ?>
+
+                                             
+                                            
+                                          
+                                          <?php
                                           }
 
                                     ?>
