f8cb1b9a84ebd004bfd91aae61a9ea605b9a2e0c
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-26 16:50:09 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,16 +7,16 @@
                 
                 <div class="row ">
                         <div class="col-md-4 grid-margin stretch-card">
-                            <div class="card" style="background-color: #343a40;color: #fff " >
+                            <div class="card" style="background-color: #343a40; " >
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
-                                        <h6 class="card-title mb-0 ">Clubs Senior</h6>
+                                        <h6 class="card-title mb-0 " style="color: #fff" >Clubs Senior</h6>
                                         <div class="dropdown mb-2">
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-6 col-md-12 col-xl-5">
-                                            <h3 class="mb-2"><?php echo $clubscountS ; ?></h3>
+                                            <h3 class="mb-2" style="color: #fff" > <?php echo $clubscountS ; ?></h3>
                                         </div>
                                     </div>
                                     <hr>
