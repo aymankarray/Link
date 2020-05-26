6bd63f5823f0376093a57c1cfb6a89a42c81964f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-26 18:55:00 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="path/to/chartjs/dist/Chart.min.css">
+
 
 <div id="content-page" class="content-page">
 <div class="container">
@@ -331,7 +331,7 @@
 <div class="row">
     
     <div class="col-md-12">
-        <canvas id="myChart" width="400" height="200"></canvas>
+        <div id="chart"></div>
     </div>
 
 </div>
@@ -346,44 +346,22 @@
 </div>
 </div>
 
-<script  src="https://www.chartjs.org/dist/2.9.3/Chart.min.js"></script>
-<script>
+<script type="text/javascript">
+        var options = {
+              chart: {
+                type: 'bar'
+              },
+              series: [{
+                name: 'sales',
+                data: [30,40,45,50,49,60,70,91,125]
+              }],
+              xaxis: {
+                categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+              }
+            }
 
-            var ctx = document.getElementById('myChart');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-</script></script>
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+            chart.render();
+
+</script>
