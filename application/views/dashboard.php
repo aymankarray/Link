84f9369ce2da8b127ddbb2675e858436a9f2ac27
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-26 17:09:19 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -49,22 +49,6 @@
                             <div class="card" style="background-color: #343a40;color: #fff " >
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
-                                        <h6 class="card-title mb-0" style="color: #fff" ><i class="ri-building-2-fill mr-3"></i>Membres Senior</h6>
-                                        <div class="dropdown mb-2">
-
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-6 col-md-12 col-xl-5">
-                                            <h3 class="mb-2" style="color: #fff"><?php echo $tunimateursApp ;?></h3>
-                                        </div>
-                                        <div class="col-6 col-md-12 col-xl-7">
-                                            <div id="sparklinePie"></div>
-                                        </div>
-
-                                    </div>
-                                    <hr>
-                                    <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0" style="color: #fff"><i class="ri-home-smile-2-fill mr-3"></i>Membres Junior</h6>
                                         <div class="dropdown mb-2">
 
@@ -80,6 +64,23 @@
 
                                     </div>
                                     <hr>
+                                    <div class="d-flex justify-content-between align-items-baseline">
+                                        <h6 class="card-title mb-0" style="color: #fff" ><i class="ri-building-2-fill mr-3"></i>Membres Senior</h6>
+                                        <div class="dropdown mb-2">
+
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6 col-md-12 col-xl-5">
+                                            <h3 class="mb-2" style="color: #fff"><?php echo $tunimateursApp ;?></h3>
+                                        </div>
+                                        <div class="col-6 col-md-12 col-xl-7">
+                                            <div id="sparklinePie"></div>
+                                        </div>
+
+                                    </div>
+                                    <hr>
+
                                     <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0" style="color: #fff"><i class="ri-building-3-fill mr-3"></i> Membres Alumni</h6>
                                         <div class="dropdown mb-2">
