9f351ba2012f690261d221dfdd06632bf2ad9c0e
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-26 16:48:21 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,7 +7,7 @@
                 
                 <div class="row ">
                         <div class="col-md-4 grid-margin stretch-card">
-                            <div class="card" style="background-color: #343a40 ">
+                            <div class="card" style="background-color: #343a40;color: #fff " >
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0 ">Clubs Senior</h6>
@@ -35,7 +35,7 @@
                         </div>
 
                         <div class="col-md-4 grid-margin stretch-card">
-                            <div class="card">
+                            <div class="card" style="background-color: #343a40;color: #fff " >
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0">Membres Senior</h6>
@@ -73,7 +73,7 @@
                         </div>
 
                         <div class="col-md-4 grid-margin stretch-card">
-                            <div class="card">
+                            <div class="card" style="background-color: #343a40;color: #fff " >
                                 <div class="card-body">
                                     <div class="d-flex justify-content-between align-items-baseline">
                                         <h6 class="card-title mb-0">Membres Alumni</h6>
