5b20d1daa9236078656accf43e52c003cb2eacdb
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-27 13:41:15 +0100

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -402,15 +402,15 @@
 <script type="text/javascript">
          var options3 = {
           series: [{
-              name: 'Net Profit',
+            <?php foreach ($projetsStatTypeDate as $key ) { ?>
+
+
+              name: ' <?php echo $key->type  ?>',
               data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-            }, {
-              name: 'Revenue',
-              data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-            }, {
-              name: 'Free Cash Flow',
-              data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-            }],
+            },
+
+            <?php } ?>
+            ],
               chart: {
               type: 'bar',
               height: 350
