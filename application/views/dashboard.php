0f73b3bc07401fad5a4171718220270512999f1a
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-27 18:04:26 +0100

    stats

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -330,10 +330,12 @@
 
 <div class="row">
     
+    <h4>Projets/Mois</h4>    
     <div class="col-md-12">
         <div id="chart"></div>
     </div>
 
+    <h4>Type de projet / Mois</h4>
     <div class="col-md-12">
         <div id="chart3"></div>
     </div>
