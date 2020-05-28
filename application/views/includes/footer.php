3da6487d6c53156b5a502042ac1042f670c6f579
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-28 20:25:22 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -52,6 +52,7 @@
       <!-- Custom JavaScript -->
       <script  src="<?php echo base_url() ;  ?>assets/js/custom.js"></script>
       <script src="<?php echo base_url() ;  ?>assets/js/dropify.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
       <script type="text/javascript">
          
             $(document).ready(function(){
