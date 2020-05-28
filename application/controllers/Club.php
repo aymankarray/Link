ef9d81c7fd3a7a80f565b076524b22d8f3b7b83f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-29 00:13:13 +0100

    Update Club.php

--- application/controllers/Club.php
+++ application/controllers/Club.php
@@ -131,7 +131,7 @@ class Club extends BaseController {
 		        }
 
 
-		        public function ClassementMembers($clubId)
+		        public function editClub($clubId)
 		        {
 					$this->load->model('user_model');
 
