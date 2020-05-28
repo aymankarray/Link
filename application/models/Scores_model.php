bd597ade81f0e6a6f1debb3992f090021edc812b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-28 15:52:53 +0100

    Update Scores_model.php

--- application/models/Scores_model.php
+++ application/models/Scores_model.php
@@ -28,26 +28,7 @@ class Scores_model extends CI_Model
     }
 
 
-        /**
-     * This function is used to get the user listing count
-     * @param string $searchText : This is optional search text
-     * @param number $page : This is pagination offset
-     * @param number $segment : This is pagination limit
-     * @return array $result : This is result
-     */
-    function RaitingClub()
-    {
-        $this->db->select('BaseTbl.score_clubID , max(BaseTbl.createdDate) createdDate ,  BaseTbl.clubID , Clubs.name , Moy(score) as scores ');
-        $this->db->from('tbl_score_club as  BaseTbl');
-        $this->db->join('tbl_club as Clubs', 'Clubs.clubID = BaseTbl.clubID','left');
-        $this->db->order_by('scores', 'DESC');
-        $this->db->group_by('Clubs.region'); 
-     
-        $query = $this->db->get();
-        
-        $result = $query->result();        
-        return $result;
-    }
+ 
 
 
 
