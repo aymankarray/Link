fb2551c6486dd97ea1baecd271eb699587ebe6e5
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-29 22:31:55 +0100

    Update Login_model.php

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -150,7 +150,7 @@ class Login_model extends CI_Model
      */
     function lastLogins()
     {
-        $this->db->select(' Users.avatar , Users.name  , BaseTbl.platform , BaseTbl.createdDtm ');
+        $this->db->select('Max(BaseTbl.id) , Users.avatar , Users.name  , BaseTbl.platform , BaseTbl.createdDtm ');
         $this->db->from('tbl_last_login as BaseTbl ');
         $this->db->join('tbl_users as Users', 'Users.userId = BaseTbl.userId','left');
         $this->db->where(' BaseTbl.createdDtm >= ADDTIME(NOW() , - 30000)  ');
