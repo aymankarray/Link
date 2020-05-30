f81fbc3f7f4cdebc8ffa9807c466b37f5c63970f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-05-30 15:25:29 +0100

    Update old.php

--- application/views/project/old.php
+++ application/views/project/old.php
@@ -162,7 +162,7 @@
 
          <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
   
-          <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+          <script defer type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 
           <script type="text/javascript">
             $(document).ready( function () {
