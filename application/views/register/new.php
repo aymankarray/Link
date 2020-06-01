46ae2e5da88da973c6468407e60eacedfe766775
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-01 22:11:24 +0100

    Update new.php

--- application/views/register/new.php
+++ application/views/register/new.php
@@ -220,38 +220,82 @@
         </section>
         <!-- Sign in END -->
       <!-- Optional JavaScript -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="<?php echo base_url() ;  ?>assets/js//jquery.min.js"></script>
-      <script src="<?php echo base_url() ;  ?>assets/js//popper.min.js"></script>
-      <script src="<?php echo base_url() ;  ?>assets/js//bootstrap.min.js"></script>
+ <script    src="<?php echo base_url() ;  ?>assets/js/jquery.min.js"></script>
+      <script   src="<?php echo base_url() ;  ?>assets/js/popper.min.js"></script>
+      <script    src="<?php echo base_url() ;  ?>assets/js/bootstrap.min.js"></script>
       <!-- Appear JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//jquery.appear.js"></script>
+      <script   src="<?php echo base_url() ;  ?>assets/js/jquery.appear.js"></script>
       <!-- Countdown JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//countdown.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/countdown.min.js"></script>
       <!-- Counterup JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//waypoints.min.js"></script>
-      <script src="<?php echo base_url() ;  ?>assets/js//jquery.counterup.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/waypoints.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/jquery.counterup.min.js"></script>
       <!-- Wow JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//wow.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/wow.min.js"></script>
       <!-- Apexcharts JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//apexcharts.js"></script>
-      <!-- lottie JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//lottie.js"></script>
-      <!-- Slick JavaScript --> 
-      <script src="<?php echo base_url() ;  ?>assets/js//slick.min.js"></script>
+      <script defer  src="<?php echo base_url() ;  ?>assets/js/apexcharts.js"></script>
+      <!-- Slick JavaScript -->
+      <script  src="<?php echo base_url() ;  ?>assets/js/slick.min.js"></script>
       <!-- Select2 JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//select2.min.js"></script>
+      <script   src="<?php echo base_url() ;  ?>assets/js/select2.min.js"></script>
       <!-- Owl Carousel JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//owl.carousel.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/owl.carousel.min.js"></script>
       <!-- Magnific Popup JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//jquery.magnific-popup.min.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/jquery.magnific-popup.min.js"></script>
       <!-- Smooth Scrollbar JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//smooth-scrollbar.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/smooth-scrollbar.js"></script>
+      <!-- lottie JavaScript -->
+      <script  src="<?php echo base_url() ;  ?>assets/js/lottie.js"></script>
       <!-- Chart Custom JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//chart-custom.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/chart-custom.js"></script>
       <!-- Custom JavaScript -->
-      <script src="<?php echo base_url() ;  ?>assets/js//custom.js"></script>
+      <script  src="<?php echo base_url() ;  ?>assets/js/custom.js"></script>
+      <script src="<?php echo base_url() ;  ?>assets/js/dropify.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+      <script type="text/javascript">
+         
+            $(document).ready(function(){
+                // Basic
+                $('.dropify').dropify();
+
+                // Translated
+                $('.dropify-fr').dropify({
+                    messages: {
+                        default: 'Glissez-déposez un fichier ici ou cliquez',
+                        replace: 'Glissez-déposez un fichier ou cliquez pour remplacer',
+                        remove:  'Supprimer',
+                        error:   'Désolé, le fichier trop volumineux'
+                    }
+                });
+
+                // Used events
+                var drEvent = $('#input-file-events').dropify();
+
+                drEvent.on('dropify.beforeClear', function(event, element){
+                    return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+                });
+
+                drEvent.on('dropify.afterClear', function(event, element){
+                    alert('File deleted');
+                });
+
+                drEvent.on('dropify.errors', function(event, element){
+                    console.log('Has Errors');
+                });
+
+                var drDestroy = $('#input-file-to-destroy').dropify();
+                drDestroy = drDestroy.data('dropify')
+                $('#toggleDropify').on('click', function(e){
+                    e.preventDefault();
+                    if (drDestroy.isDropified()) {
+                        drDestroy.destroy();
+                    } else {
+                        drDestroy.init();
+                    }
+                })
+            });
+        
+      </script>
        <script>
 
 
