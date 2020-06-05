00d6d4c01d751683c8688ded6854a7b39aa99d3b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-05 16:11:11 +0100

    Update list.php

--- application/views/academy/list.php
+++ application/views/academy/list.php
@@ -34,7 +34,7 @@
                               
                            <div class="slick-list draggable" style="padding: 0px 50px;"><div class="slick-track" style="opacity: 1; width: 3870px; transform: translate3d(-1075px, 0px, 0px);"><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="-2" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -43,7 +43,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="-1" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -52,7 +52,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="0" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -61,7 +61,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="1" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n2.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n2.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -70,7 +70,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="2" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n3.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n3.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -79,7 +79,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-current slick-active slick-center" tabindex="0" style="width: 215px;" data-slick-index="3" aria-hidden="false">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="0"><img src="images/page-img/n4.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="0"><img src="<?php echo base_url()  ?>assets/images/page-img/n4.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="0"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -88,7 +88,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="4" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n5.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n5.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -97,7 +97,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="5" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n6.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n6.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -106,7 +106,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="6" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -115,7 +115,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide" tabindex="-1" style="width: 215px;" data-slick-index="7" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -124,7 +124,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="8" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -133,7 +133,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="9" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n2.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n2.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -142,7 +142,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="10" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n3.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n3.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -151,7 +151,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="11" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n4.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n4.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -160,7 +160,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="12" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n5.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n5.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -169,7 +169,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="13" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n6.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n6.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -178,7 +178,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="14" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -187,7 +187,7 @@
                                  <p class="mb-0">Best Advice</p>
                               </li><li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 215px;" data-slick-index="15" aria-hidden="true">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#" tabindex="-1"><img src="images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
                                     </div>
