5312076437221f9a17535ac249f7ce13cfbdbc76
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-05 16:48:55 +0100

    Update list.php

--- application/views/academy/list.php
+++ application/views/academy/list.php
@@ -25,12 +25,11 @@
                               <a href="#"> </a>
                            </div>
                         </div>
-                        <div class="iq-card-body">
-                          
-                        	<ul id="new-music" class="d-flex list-inline m-0 p-0">
+						<div class="iq-card-body">
+                           <ul id="new-music" class="d-flex list-inline m-0 p-0">
                               <li class="text-center col-sm-2">
                                  <div class="music-thumbnail position-relative mb-3">
-                                    <a href="#"><img src="<?php echo base_url()  ?>assets/images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <a href="#"><img src="images/page-img/n1.jpg" alt="music-thumb" class="img-fluid w-100"></a>
                                     <div class="play-btn">
                                        <a href="#"><i class="ri-play-fill text-white"></i></a>
                                     </div>
@@ -38,11 +37,81 @@
                                  <h6>Sean Paul</h6>
                                  <p class="mb-0">Best Advice</p>
                               </li>
-                          </ul>
-
-
-
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n2.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Fetty Wap</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n3.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Brittany Howard</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n4.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Elton John</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n5.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Tierra Whack</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n6.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Trippie Redd</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n7.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Quincy Jones</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                              <li class="text-center col-sm-2">
+                                 <div class="music-thumbnail position-relative mb-3">
+                                    <a href="#"><img src="images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100"></a>
+                                    <div class="play-btn">
+                                       <a href="#"><i class="ri-play-fill text-white"></i></a>
+                                    </div>
+                                 </div>
+                                 <h6>Ciara</h6>
+                                 <p class="mb-0">Best Advice</p>
+                              </li>
+                           </ul>
                         </div>
+
+
+                        
                      </div>
                   </div>
                   
