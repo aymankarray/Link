3301a15ca127f30a6afd3cd9a8318b99d1d4f926
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-05 16:32:42 +0100

    Update list.php

--- application/views/academy/list.php
+++ application/views/academy/list.php
@@ -56,7 +56,16 @@
 							                <p class="mb-0">Best Advice</p>
 							            </li>
 							            
-							            
+							            <li class="text-center col-sm-2 slick-slide slick-cloned" tabindex="-1" style="width: 157px;" data-slick-index="15" aria-hidden="true">
+							                <div class="music-thumbnail position-relative mb-3">
+							                    <a href="#" tabindex="-1"><img src="<?php echo base_url()  ?>assets/images/page-img/n8.jpg" alt="music-thumb" class="img-fluid w-100" /></a>
+							                    <div class="play-btn">
+							                        <a href="#" tabindex="-1"><i class="ri-play-fill text-white"></i></a>
+							                    </div>
+							                </div>
+							                <h6>Ciara</h6>
+							                <p class="mb-0">Best Advice</p>
+							            </li>
 							        </div>
 							    </div>
 							</ul>
