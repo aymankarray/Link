8536331e91cb564d45c2f100d87a95de9f75e5a2
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-05 17:34:26 +0100

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -21,7 +21,7 @@
       <!-- Footer END -->
       <!-- Optional JavaScript -->
       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script  async  src="<?php echo base_url() ;  ?>assets/js/jquery.min.js"></script>
+      <script    src="<?php echo base_url() ;  ?>assets/js/jquery.min.js"></script>
       <script   src="<?php echo base_url() ;  ?>assets/js/popper.min.js"></script>
       <script    src="<?php echo base_url() ;  ?>assets/js/bootstrap.min.js"></script>
       <!-- Appear JavaScript -->
