36cca881b5d6e629e4b7aecc205fca4761e311ec
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-05 16:58:56 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -786,7 +786,7 @@
                      <li>
                         <a href="#mailbox" class="iq-waves-effect" data-toggle="collapse" aria-expanded="true">
                             <span class="ripple rippleEffect" style="width: 190px; height: 190px; top: -67px; left: 70px;"> </span>
-                            <i class="ri-mail-line"></i><span>Academy</span><i class="ri-arrow-right-s-line iq-arrow-right">Soon</i>
+                            <i class="ri-mail-line"></i><span>Academy<span class="iq-arrow-right">Soon<i class="ri-arrow-right-s-line "></i></span>
                         </a>
                         <ul id="mailbox" class="iq-submenu collapse show" data-parent="#iq-sidebar-toggle" style="">
                             <li>
