c9aed2ca26e3b906e424fee402beae32eccf3cd2
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 17:54:41 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -30,35 +30,7 @@
 
                                                
 
-                                              <script defer >
-
-                                                  // create youtube player
-                                                  var player;
-                                                  function onYouTubePlayerAPIReady() {
-                                                      player = new YT.Player('player', {
-                                                        width: '640',
-                                                        height: '390',
-                                                        videoId: '0Bmhjf0rKe8',
-                                                        events: {
-                                                          onReady: onPlayerReady,
-                                                          onStateChange: onPlayerStateChange
-                                                        }
-                                                      });
-                                                  }
-
-                                                  // autoplay video
-                                                  function onPlayerReady(event) {
-                                                      event.target.playVideo();
-                                                  }
-
-                                                  // when video ends
-                                                  function onPlayerStateChange(event) {        
-                                                      if(event.data === 0) {          
-                                                          alert('done');
-                                                      }
-                                                  }
-
-                                              </script>
+                                             
                                                 
                                              
                                           </div>
