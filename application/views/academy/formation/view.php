0134fb0a7884b9709e00a7d513a9a4ba8d81b99e
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 21:14:38 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -159,88 +159,92 @@
 
                         </div>
 
-                        <div class="row">      
-                                          <div class="col-md-8">
-                                             <h5>Note</h5>
-                                              
-                                             <?php echo $key->description ;?>
-                                              
-                                          </div>
-                                          <div class="col-md-4">
-                                             <div class="row">
-                                                 <span class="heading">User Rating</span>
-                                                 <span class="fa fa-star checked"></span>
-                                                 <span class="fa fa-star checked"></span>
-                                                 <span class="fa fa-star checked"></span>
-                                                 <span class="fa fa-star checked"></span>
-                                                 <span class="fa fa-star"></span>
-                                              </div>
-                                                   <p>4.1 average based on 254 reviews.</p>
-                                                   <hr style="border:1px solid #f1f1f1">
 
+                        <div class="card">
+                           <div class="iq-card-body">
+                              <div class="row">      
+                                                <div class="col-md-8">
+                                                   <h5>Note</h5>
+                                                    
+                                                   <?php echo $key->description ;?>
+                                                    
+                                                </div>
+                                                <div class="col-md-4">
                                                    <div class="row">
-                                                     <div class="side">
-                                                       <div>5 star</div>
-                                                     </div>
-                                                     <div class="middle">
-                                                       <div class="bar-container">
-                                                         <div class="bar-5"></div>
-                                                       </div>
-                                                     </div>
-                                                     <div class="side right">
-                                                       <div>150</div>
-                                                     </div>
-                                                     <div class="side">
-                                                       <div>4 star</div>
-                                                     </div>
-                                                     <div class="middle">
-                                                       <div class="bar-container">
-                                                         <div class="bar-4"></div>
-                                                       </div>
-                                                     </div>
-                                                     <div class="side right">
-                                                       <div>63</div>
-                                                     </div>
-                                                     <div class="side">
-                                                       <div>3 star</div>
-                                                     </div>
-                                                     <div class="middle">
-                                                       <div class="bar-container">
-                                                         <div class="bar-3"></div>
-                                                       </div>
-                                                     </div>
-                                                     <div class="side right">
-                                                       <div>15</div>
-                                                     </div>
-                                                     <div class="side">
-                                                       <div>2 star</div>
-                                                     </div>
-                                                     <div class="middle">
-                                                       <div class="bar-container">
-                                                         <div class="bar-2"></div>
-                                                       </div>
-                                                     </div>
-                                                     <div class="side right">
-                                                       <div>6</div>
-                                                     </div>
-                                                     <div class="side">
-                                                       <div>1 star</div>
-                                                     </div>
-                                                     <div class="middle">
-                                                       <div class="bar-container">
-                                                         <div class="bar-1"></div>
-                                                       </div>
-                                                     </div>
-                                                     <div class="side right">
-                                                       <div>20</div>
-                                                     </div>
-                                                   </div>
+                                                       <span class="heading">User Rating</span>
+                                                       <span class="fa fa-star checked"></span>
+                                                       <span class="fa fa-star checked"></span>
+                                                       <span class="fa fa-star checked"></span>
+                                                       <span class="fa fa-star checked"></span>
+                                                       <span class="fa fa-star"></span>
+                                                    </div>
+                                                         <p>4.1 average based on 254 reviews.</p>
+                                                         <hr style="border:1px solid #f1f1f1">
+
+                                                         <div class="row">
+                                                           <div class="side">
+                                                             <div>5 star</div>
+                                                           </div>
+                                                           <div class="middle">
+                                                             <div class="bar-container">
+                                                               <div class="bar-5"></div>
+                                                             </div>
+                                                           </div>
+                                                           <div class="side right">
+                                                             <div>150</div>
+                                                           </div>
+                                                           <div class="side">
+                                                             <div>4 star</div>
+                                                           </div>
+                                                           <div class="middle">
+                                                             <div class="bar-container">
+                                                               <div class="bar-4"></div>
+                                                             </div>
+                                                           </div>
+                                                           <div class="side right">
+                                                             <div>63</div>
+                                                           </div>
+                                                           <div class="side">
+                                                             <div>3 star</div>
+                                                           </div>
+                                                           <div class="middle">
+                                                             <div class="bar-container">
+                                                               <div class="bar-3"></div>
+                                                             </div>
+                                                           </div>
+                                                           <div class="side right">
+                                                             <div>15</div>
+                                                           </div>
+                                                           <div class="side">
+                                                             <div>2 star</div>
+                                                           </div>
+                                                           <div class="middle">
+                                                             <div class="bar-container">
+                                                               <div class="bar-2"></div>
+                                                             </div>
+                                                           </div>
+                                                           <div class="side right">
+                                                             <div>6</div>
+                                                           </div>
+                                                           <div class="side">
+                                                             <div>1 star</div>
+                                                           </div>
+                                                           <div class="middle">
+                                                             <div class="bar-container">
+                                                               <div class="bar-1"></div>
+                                                             </div>
+                                                           </div>
+                                                           <div class="side right">
+                                                             <div>20</div>
+                                                           </div>
+                                                         </div>
+                                                    
                                               
-                                        
+                                               </div>
                                          </div>
-                                   </div>
 
-
+                                 </div>
+                              </div>
          
          </div>
       </div>
