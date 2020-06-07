99f92c8796162ffa2c72b83a5aac1f6fef73ccaf
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 19:30:32 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -2,48 +2,29 @@
                                 
 <style>
 
-/* Style the tab */
-.tab {
+/* Style tab links */
+.tablink {
+  background-color: #555;
+  color: white;
   float: left;
-  border: 1px solid #ccc;
-  background-color: #f1f1f1;
-  width: 30%;
- 
-}
-
-/* Style the buttons inside the tab */
-.tab button {
-  display: block;
-  background-color: inherit;
-  color: black;
-  padding: 22px 16px;
-  width: 100%;
   border: none;
   outline: none;
-  text-align: left;
   cursor: pointer;
-  transition: 0.3s;
+  padding: 14px 16px;
   font-size: 17px;
+  width: 25%;
 }
 
-/* Change background color of buttons on hover */
-.tab button:hover {
-  background-color: #ddd;
+.tablink:hover {
+  background-color: #777;
 }
 
-/* Create an active/current "tab button" class */
-.tab button.active {
-  background-color: var(--iq-primary);
-}
-
-/* Style the tab content */
+/* Style the tab content (and add height:100% for full page content) */
 .tabcontent {
-  float: left;
-  padding: 0px 12px;
-  border: 1px solid #ccc;
-  width: 70%;
-  border-left: none;
-  height: 300px;
+  color: white;
+  display: none;
+  padding: 100px 20px;
+  height: 100%;
 }
 </style>
 
@@ -64,9 +45,13 @@
 
                               <div class="tab">
                                     <?php foreach ($chapters as $key ) {  ?>
-                                   <button class="tablinks" onclick="openCity(event, 'CH<?php echo $key->chapterId ;?>')"  <?php if ($key->num==1 ) {  ?> id="defaultOpen" <?php } ?> >
+
+                                    <button class="tablink" onclick="openPage('CH<?php echo $key->chapterId ;?>', this, 'red')" <?php if ($key->num==1 ) {  ?> id="defaultOpen" <?php } ?> >
+
                                      <?php echo $key->num ;?> - <?php echo $key->titre ;?>
-                                   </button>
+                                        
+                                     </button>
+         
                                    <?php }  ?>
                                  </div>
 
@@ -91,18 +76,25 @@
 
 
 <script>
-function openCity(evt, cityName) {
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].style.backgroundColor = "";
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
