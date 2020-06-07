5e7d11f9c8a7cbad2a8730f26c0d9d81adb334b6
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 18:14:15 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -1,12 +1,7 @@
   
-                                                  <script>
-                                                // 2. This code loads the IFrame Player API code asynchronously.
-                                                var tag = document.createElement('script');
+                                
 
-                                                tag.src = "https://www.youtube.com/iframe_api";
-                                                var firstScriptTag = document.getElementsByTagName('script')[0];
-                                                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-                                             </script>
+                                    
 
          <div id="content-page" class="content-page">
             <div class="container">
@@ -39,6 +34,12 @@
                                                 <div id="player<?php echo $key->chapterId  ; ?>"></div>
 
                                              <script type="text/javascript">
+                                                                                                // 2. This code loads the IFrame Player API code asynchronously.
+                                                var tag = document.createElement('script');
+
+                                                tag.src = "https://www.youtube.com/iframe_api";
+                                                var firstScriptTag = document.getElementsByTagName('script')[0];
+                                                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
                                                 // 3. This function creates an <iframe> (and YouTube player)
                                                 //    after the API code downloads.
                                                 var player;
