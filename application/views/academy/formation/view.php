17d9b0d954b08966431f3c50680fe8516d767b76
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 20:42:37 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -125,9 +125,10 @@
                                           
                              
                                               <iframe  class="embed-responsive-item"  src="https://www.youtube.com/embed/<?php echo $key->embed ;?>" frameborder="0" allow="" allowfullscreen=""></iframe>
-                                                <br>
+
                                          </div>
-                                         <br>
+                                    </div>
+                                    <div class="row">      
                                           <div class="card col-md-8">
                                              <div class="iq-card-header"><h5>Note</h5></div>
                                               <div class="iq-card-body">
