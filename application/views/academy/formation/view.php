18f60af29b8942e36d2ecc2e84da0d9b8116d8b9
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 18:12:51 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -1,3 +1,13 @@
+  
+                                                  <script>
+                                                // 2. This code loads the IFrame Player API code asynchronously.
+                                                var tag = document.createElement('script');
+
+                                                tag.src = "https://www.youtube.com/iframe_api";
+                                                var firstScriptTag = document.getElementsByTagName('script')[0];
+                                                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                                             </script>
+
          <div id="content-page" class="content-page">
             <div class="container">
                
@@ -27,14 +37,8 @@
                                              <div class="card-title"><?php echo $key->titre  ; ?></div>
                                              <div class="iq-card-body">                                               
                                                 <div id="player<?php echo $key->chapterId  ; ?>"></div>
-                                                <script>
-                                                // 2. This code loads the IFrame Player API code asynchronously.
-                                                var tag = document.createElement('script');
 
-                                                tag.src = "https://www.youtube.com/iframe_api";
-                                                var firstScriptTag = document.getElementsByTagName('script')[0];
-                                                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+                                             <script type="text/javascript">
                                                 // 3. This function creates an <iframe> (and YouTube player)
                                                 //    after the API code downloads.
                                                 var player;
@@ -50,11 +54,7 @@
                                                   });
                                                 }
 
-                                                // 4. The API will call this function when the video player is ready.
-                                                function onPlayerReady(event) {
-                                                  event.target.playVideo();
-                                                }
-
+                                                
                                                 // 5. The API calls this function when the player's state changes.
                                                 //    The function indicates that when playing a video (state=1),
                                                 //    the player should play for six seconds and then stop.
