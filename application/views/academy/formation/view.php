d3ec1199e7c48760ffc93bf66903a61169076917
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 17:25:00 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -14,7 +14,7 @@
                               <div class="col-sm-3">
                                  <div class="nav flex-column nav-pills text-left" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                     <?php foreach ($chapters as $key ) { ?>
-                                    <a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="#<?php echo $key->chapterId  ; ?>" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#<?php echo $key->chapterId  ; ?>" role="tab" aria-controls="v-pills-home" aria-selected="true">
                                     <?php echo $key->num  ; ?> - <?php echo $key->titre  ; ?>
                                     </a>
                                     <?php } ?>
@@ -23,7 +23,7 @@
                               <div class="col-sm-9">
                                  <div class="tab-content mt-0" id="v-pills-tabContent">
                                     <?php foreach ($chapters as $key ) { ?>
-                                       <div class="tab-pane fade show" id="<?php echo $key->chapterId  ; ?>" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                       <div class="tab-pane fade show active" id="<?php echo $key->chapterId  ; ?>" role="tabpanel" aria-labelledby="v-pills-home-tab">
                                           <?php echo $key->embed  ; ?>
                                           <hr>
                                           <h6>Description</h6>
