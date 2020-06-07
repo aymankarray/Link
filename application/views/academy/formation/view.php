3f5c0c017f2eb1a6861126e0f0ec0229afd53a72
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 20:15:41 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -125,34 +125,13 @@ document.getElementById("defaultOpen").click();
       // 3. This function creates an <iframe> (and YouTube player)
       //    after the API code downloads.
       <?php foreach ($chapters as $key ) {  ?>
-      var player<?php echo $key->chapterId ;?> ;
-      function onYouTubeIframeAPIReady() {
-        player<?php echo $key->chapterId ;?> = new YT.Player('player<?php echo $key->chapterId ;?>', {
-          videoId: '<?php echo $key->embed ;?>',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
+         var player<?php echo $key->chapterId ;?> ;
+         function onYouTubeIframeAPIReady() {
+           player<?php echo $key->chapterId ;?> = new YT.Player('player<?php echo $key->chapterId ;?>', {
+             videoId: '<?php echo $key->embed ;?>',
+           });
       }
       <?php } ?>
 
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
+      
     </script>
