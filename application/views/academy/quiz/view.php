6ab77a512081754c4438d71e1b23076d235dc614
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-08 00:38:17 +0100

    Update view.php

--- application/views/academy/quiz/view.php
+++ application/views/academy/quiz/view.php
@@ -1,7 +1,7 @@
  <div id="content-page" class="content-page">
     <div class="container">
         
-        <?php foreach ($question as $key ) { ?>
+        <?php foreach ($questions as $key ) { ?>
         
         <div class="row">
 	        <div class="iq-card">
@@ -10,46 +10,46 @@
 		        </div>
 		        <div class="iq-card-body">
 		        		
-		        		<?php if ($key->choice1) != '' { ?>
+		        		<?php if ($key->choice1 != '') { ?>
 		        			<input type="radio" value="0" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->choice1 ?> <br>
 		        		<?php } ?>
 
-		        		<?php if ($key->Answer2) != '' { ?>
+		        		<?php if ($key->Answer2 != '') { ?>
 		        			<input type="radio" value="1" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->Answer2 ?> <br>
 		        		<?php } ?>
 		        		
-		        		<?php if ($key->choice1) != '' { ?>
+		        		<?php if ($key->choice1 != '') { ?>
 		        			<input type="radio" value="0" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->choice2 ?> <br>
 		        		<?php } ?>
 
-		        		<?php if ($key->Answer1) != '' { ?>
+		        		<?php if ($key->Answer1 != '') { ?>
 		        			<input type="radio" value="1" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->Answer1 ?> <br>
 		        		<?php } ?>
 		        		
-		        		<?php if ($key->choice3) != '' { ?>
+		        		<?php if ($key->choice3 != ''  ) { ?>
 		        			<input type="radio" value="0" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->choice2 ?> <br>
 		        		<?php } ?>
 
 		        		
-		        		<?php if ($key->Answer3) != '' { ?>
+		        		<?php if ($key->Answer3 != '')  { ?>
 		        			<input type="radio" value="1" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->Answer3 ?> <br>
 		        		<?php } ?>
 
 		        		
-		        		<?php if ($key->choice4) != '' { ?>
+		        		<?php if ($key->choice4 != '')  { ?>
 		        			<input type="radio" value="0" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->choice4 ?> <br>
 		        		<?php } ?>
 
 
 		        		
-		        		<?php if ($key->Answer4) != '' { ?>
+		        		<?php if ($key->Answer4) != '' ) { ?>
 		        			<input type="radio" value="1" name="<?php echo $key->quizId ; ?>"> 
 		        			<?php echo $key->Answer4 ?> <br>
 		        		<?php } ?>
