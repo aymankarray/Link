db65578d34b50a438d2444435b3fb57d6e51ede3
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-07 20:32:40 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -27,6 +27,66 @@
 
 }
 
+
+.fa {
+  font-size: 25px;
+}
+
+.checked {
+  color: orange;
+}
+
+/* Three column layout */
+.side {
+  float: left;
+  width: 15%;
+  margin-top:10px;
+}
+
+.middle {
+  margin-top:10px;
+  float: left;
+  width: 70%;
+}
+
+/* Place text to the right */
+.right {
+  text-align: right;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* The bar container */
+.bar-container {
+  width: 100%;
+  background-color: #f1f1f1;
+  text-align: center;
+  color: white;
+}
+
+/* Individual bars */
+.bar-5 {width: 60%; height: 18px; background-color: #4CAF50;}
+.bar-4 {width: 30%; height: 18px; background-color: #2196F3;}
+.bar-3 {width: 10%; height: 18px; background-color: #00bcd4;}
+.bar-2 {width: 4%; height: 18px; background-color: #ff9800;}
+.bar-1 {width: 15%; height: 18px; background-color: #f44336;}
+
+/* Responsive layout - make the columns stack on top of each other instead of next to each other */
+@media (max-width: 400px) {
+  .side, .middle {
+    width: 100%;
+  }
+  .right {
+    display: none;
+  }
+}
+
+
 </style>
 
                                     
@@ -63,13 +123,83 @@
                                    <div class="row">
                                          <div class="card col-md-8 embed-responsive embed-responsive-16by9">
                                           
-                                           <div id="player<?php echo $key->chapterId ;?>" class="embed-responsive-item"></div>
-
-
+                             
+                                              <iframe  class="embed-responsive-item"  src="https://www.youtube.com/embed/<?php echo $key->embed ;?>" frameborder="0" allow="" allowfullscreen=""></iframe>
+                                        
                                          </div>
                                           <div class="card col-md-4">
-                                             <h6>Note</h6>
-                                              <?php echo $key->description ;?>
+                                             <div class="iq-card-header"><h6>Note</h6></div>
+                                              <div class="iq-card-body">
+                                                 <?php echo $key->description ;?>
+                                              </div>
+                                              <div class="iq-card-footer" >
+                                                 <span class="heading">User Rating</span>
+                                                 <span class="fa fa-star checked"></span>
+                                                 <span class="fa fa-star checked"></span>
+                                                 <span class="fa fa-star checked"></span>
+                                                 <span class="fa fa-star checked"></span>
+                                                 <span class="fa fa-star"></span>
+                                                   <p>4.1 average based on 254 reviews.</p>
+                                                   <hr style="border:3px solid #f1f1f1">
+
+                                                   <div class="row">
+                                                     <div class="side">
+                                                       <div>5 star</div>
+                                                     </div>
+                                                     <div class="middle">
+                                                       <div class="bar-container">
+                                                         <div class="bar-5"></div>
+                                                       </div>
+                                                     </div>
+                                                     <div class="side right">
+                                                       <div>150</div>
+                                                     </div>
+                                                     <div class="side">
+                                                       <div>4 star</div>
+                                                     </div>
+                                                     <div class="middle">
+                                                       <div class="bar-container">
+                                                         <div class="bar-4"></div>
+                                                       </div>
+                                                     </div>
+                                                     <div class="side right">
+                                                       <div>63</div>
+                                                     </div>
+                                                     <div class="side">
+                                                       <div>3 star</div>
+                                                     </div>
+                                                     <div class="middle">
+                                                       <div class="bar-container">
+                                                         <div class="bar-3"></div>
+                                                       </div>
+                                                     </div>
+                                                     <div class="side right">
+                                                       <div>15</div>
+                                                     </div>
+                                                     <div class="side">
+                                                       <div>2 star</div>
+                                                     </div>
+                                                     <div class="middle">
+                                                       <div class="bar-container">
+                                                         <div class="bar-2"></div>
+                                                       </div>
+                                                     </div>
+                                                     <div class="side right">
+                                                       <div>6</div>
+                                                     </div>
+                                                     <div class="side">
+                                                       <div>1 star</div>
+                                                     </div>
+                                                     <div class="middle">
+                                                       <div class="bar-container">
+                                                         <div class="bar-1"></div>
+                                                       </div>
+                                                     </div>
+                                                     <div class="side right">
+                                                       <div>20</div>
+                                                     </div>
+                                                   </div>
+                                              </div>
                                         
                                          </div>
                                    </div>
@@ -112,31 +242,3 @@ function openPage(pageName, elmnt, color) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 </script>
-
-<script>
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-</script>
-
-
-
- <?php foreach ($chapters as $key ) {  ?>
-<script >
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-     
-         var player<?php echo $key->chapterId ;?> ;
-         function onYouTubeIframeAPIReady() {
-           player<?php echo $key->chapterId ;?> = new YT.Player('player<?php echo $key->chapterId ;?>', {
-             videoId: '<?php echo $key->embed ;?>',
-           });
-      }
-
-
-      
-    </script>     
- <?php } ?>
