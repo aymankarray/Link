4b6ced10638a0dd2fca32231e9e47b65ca79e73d
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-12 17:34:00 +0100

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -861,11 +861,11 @@ class User extends BaseController
     {
         $data["userInfo"] = $this->user_model->getUserInfoWithRole($userId);
         $data["ressourceInfo"] = $this->ressource_model->ressourceListingBUser($userId);
-        $data  ['postRecords'] =  $this->posts_model->postsListingbyUser($userId);
+        $data['postRecords'] =  $this->posts_model->postsListingbyUser($userId);
 
         $data["Experience"] = $this->user_cariere_model->carrierListing($userId);   
-        $data["HSkils"] = $this->ressource_model->carrierListing($userId); 
-        $data["Diplome"] = $this->ressource_model->carrierListing($userId);
+        $data["HSkils"] = ""; 
+        $data["Diplome"] = "";
 
         foreach ($data['postRecords'] as $key ) {                
             $key->commentsRecords= $this->posts_model->CommentsListing($key->postId);
