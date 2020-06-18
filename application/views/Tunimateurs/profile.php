501bc4f5720795f8d234b71c354fc267cd9c62ee
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-18 21:13:58 +0100

    Update profile.php

--- application/views/Tunimateurs/profile.php
+++ application/views/Tunimateurs/profile.php
@@ -160,7 +160,7 @@
                                                    <div class="event-post position-relative">
                                                       <div class="iq-card-body  p-2">
                                                          <h6>
-                                                            <i class="ri-briefcase-line"></i> <?php echo $key->type  ?> <?php echo $key->titre ?> de  
+                                                            <i class="ri-briefcase-line"></i> <b><?php echo $key->type  ?> <?php echo $key->titre ?></b> à   
                                                             <a class="text-primary" ><?php echo $key->instituts  ?> </a>
                                                              
                                                          </h6>
