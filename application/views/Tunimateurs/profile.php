66ae4300c9bf242ecba48e2fa02ccdc32bcb9ad0
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-18 21:11:04 +0100

    Update profile.php

--- application/views/Tunimateurs/profile.php
+++ application/views/Tunimateurs/profile.php
@@ -160,7 +160,8 @@
                                                    <div class="event-post position-relative">
                                                       <div class="iq-card-body  p-2">
                                                          <h6>
-                                                            <i class="ri-briefcase-line"></i> <?php echo $key->type  ?> <?php echo $key->titre ?> de  <a class="text-primary" href="<?php echo base_url() ?>club/clubInfo/<?php echo  $key->clubId ?>"><?php echo $key->instituts  ?> </a>
+                                                            <i class="ri-briefcase-line"></i> <?php echo $key->type  ?> <?php echo $key->titre ?> de  
+                                                            <a class="text-primary" ><?php echo $key->instituts  ?> </a>
                                                              
                                                          </h6>
                                                          <p><?php echo $key->dateDebut?> - <?php echo $key->dateFin  ?></p>
