cd3fafe7507d59ee60058c8eec4fdbed6805fa5d
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-20 14:29:05 +0100

    Update Passation.php

--- application/controllers/Passation.php
+++ application/controllers/Passation.php
@@ -37,7 +37,7 @@ class Passation extends BaseController {
 
 		                $data["Experience"] = $this->user_cariere_model->carrierListing($this->vendorId);   
 		           		$data["Diplome"] = $this->user_diplome_model->diplomeListing($this->vendorId) ;
-		           		$data["ressourceInfo"] = $this->ressource_model->ressourceListingBUser($userId);
+		           		$data["ressourceInfo"] = $this->ressource_model->ressourceListingBUser($this->vendorId);
 		           		
 		        		$this->loadViews("club/passation/new", $this->global, $data, NULL);  
 		        		  
