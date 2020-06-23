76eec4820fb50ea90881cadf12795a48e47917cb
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-23 15:44:12 +0100

    Update new.php

--- application/views/club/passation/new.php
+++ application/views/club/passation/new.php
@@ -69,7 +69,7 @@
                             <div class="col-sm-6">
 
                             <h4><br>Experience Tunivisions :</h4>
-                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a href="<?php echo base_url() ?>User/CarierShow/<?php echo $uid ?>"> mettre à jour </a> <br> </small>
+                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a data-toggle="modal" data-target="#experience" > mettre à jour </a> <br> </small>
                               <?php foreach ($Experience as $key) {   ?>
                                                <div class="col-sm-12">
                                                   <div class="event-post position-relative">
@@ -90,7 +90,7 @@
                                                </div>
                               <?php }  ?>
                             <h4><br>Autres activités associatives :</h4>
-                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a href="<?php echo base_url() ?>User/CarierShow/<?php echo $uid ?>"> mettre à jour </a> <br> </small>
+                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le  <a data-toggle="modal" data-target="#Diplome" > mettre à jour </a> <br> </small>
                                <?php foreach ($ExperienceA as $key) {   ?>
                                                 <div class="col-sm-12">
                                                    <div class="event-post position-relative">
@@ -109,7 +109,7 @@
 
 
                               <h4><br>Diplomes :</h4>
-                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a href="<?php echo base_url() ?>User/CarierShow/<?php echo $uid ?>"> mettre à jour </a> <br> </small>
+                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a data-toggle="modal" data-target="#experienceAutre" > mettre à jour </a> <br> </small>
                                <?php foreach ($Diplome as $key) {   ?>
                                                 <div class="col-sm-12">
                                                    <div class="event-post position-relative">
@@ -131,7 +131,7 @@
 
 
                             <h4><br>Activités professionelle :</h4>
-                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le <a href="<?php echo base_url() ?>User/CarierShow/<?php echo $uid ?>"> mettre à jour </a> <br> </small>
+                            <small class="text-muted ">On va prendre une copie de votre profile comme étant un cv merci de le  <a data-toggle="modal" data-target="#experiencePro" > mettre à jour </a> <br> </small>
                                <?php foreach ($ExperienceP as $key) {   ?>
                                                 <div class="col-sm-12">
                                                    <div class="event-post position-relative">
@@ -193,16 +193,6 @@
                             
                           </div>
 
-                    
-                            
-                          
-
-                            
-                            
-                           
-
-                           
-         
                         <br>
     
                         <div class="box-footer">
@@ -221,3 +211,138 @@
             </div>
          </div>
 
+
+               <!--  experience   -->
+         <div class="modal fade" id="experience" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="display: none;">
+            <div class="modal-dialog" role="document">
+               <div class="modal-content">
+                  <form action="<?php echo base_url() ?>Cariere/CarierAdd/<?php echo $uid ?>" method="post">
+                  <div class="modal-header">
+                     <h5 class="modal-title" id="exampleModalLabel">Nouvelle experience</h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">×</span>
+                     </button>
+                  </div>
+                  <div class="modal-body">
+
+                        <label>Debut</label>
+                        <input type="number"  name="attacheDT" id="attacheDT"  class="form-control" min="2016" max="2020" required>
+                        <label>Fin</label>
+                        <input type="number" min="2017" max="2020"   name="endDT" id="endDT" class="form-control"  required>
+                     
+               
+                        <label>Poste</label>
+                        <select name="roleId" id="roleId"  class="form-control" required >
+                           <option value="-1" >Veuillez choisir votre poste </option>
+                           <?php foreach ($Roles as $key ) {
+                            echo "<option value='".$key->roleId."'>".$key->role."</option>" ; 
+                           }?>
+                        </select>
+                        <br>
+                        <label>Cellule</label>
+                        <select name="cellule" id="cellule"  class="form-control" >
+                           <option value="-1" >Veuillez choisir votre unité </option>
+                           <option value="" ></option>
+                           <option value="Marketing" >Marketing</option>
+                           <option value="Administration et finance" >Administration et finance</option>
+                           <option value="Evenementiel" >Evenementiel</option>
+                           <option value="Gestion des talents" >Gestion des talents</option>
+                        </select>
+                        <br>
+                        <label>Club</label>
+                        <select name="clubId" id="clubId"   class="form-control" required >
+                           <option value="-1" >Veuillez choisir votre club </option>
+                           <?php foreach ($Clubs as $key ) {
+                            echo "<option value='".$key->clubID."'>".$key->name."</option>" ;  
+                           }?>
+                        </select>
+
+
+                     
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                     <input id="TF" type="submit"  class="btn btn-primary">
+                  </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      <!-- -->
+
+
+
+      <!--  diplome   -->
+         <div class="modal fade" id="diplome" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="display: none;">
+            <div class="modal-dialog" role="document">
+               <div class="modal-content">
+                  <form  method="post" action="<?php echo base_url() ?>Cariere/DiplomeAdd/<?php echo $uid ?>" >
+                  <div class="modal-header">
+                     <h5 class="modal-title" id="exampleModalLabel">ajouter un Diplome</h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">×</span>
+                     </button>
+                  </div>
+                  <div class="modal-body">
+
+                        <label>Debut</label>
+                        <input type="number"  name="dateDebut" id="dateDebut"  class="form-control" min="2010" max="2020" required>
+                        <label>Fin</label>
+                        <input type="number" min="2010" max="2020" name="dateFin" id="dateFin" class="form-control"  required>
+                     
+               
+                        <label>Type de diplome : </label>
+                        <select name="type" id="type"  class="form-control" required >
+                           <option value="" >Veuillez choisir votre poste </option>
+                           <option value="Licence appliquée" >Licence appliquée</option>
+                           <option value="Licence fondamentale" >Licence fondamentale</option>
+                           <option value="Master professionnel" >Master professionnel</option>
+                           <option value="Master de recherche" >Master de recherche</option>
+                           <option value="Ingénieurie" >Ingénieurie</option>
+                        </select>
+                        <br>
+                        <label>Titre : </label>
+                        <input type="text"   class="form-control" name="titre" >
+                        <br>
+                        <label>Ecole : </label>
+                        <select name="institut" id="institut"   class="form-control" required >
+                           <option value="-1" >Veuillez choisir votre école </option>
+                           <?php foreach ($Clubs as $key ) {
+                            echo "<option value='".$key->clubID."'>".$key->name."</option>" ;  
+                           }?>
+                        </select>
+
+
+                     
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                     <input id="TF" type="submit"  class="btn btn-primary">
+                  </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      <!-- -->
+
+
+
+          <script type="text/javascript">
+          $( document ).ready( function()
+          {
+              var roleId = $( '#roleId' );
+              var cellule = $( '#cellule' );
+              var clubId = $( '#clubId' );
+              var institut = $( '#institut' );
+              var institut = $( '#type' );
+
+              
+             $('#roleId').select2();
+             $('#cellule').select2();
+             $('#clubId').select2();
+             $('#type').select2();
+             $('#institut').select2();
+
+         });
+
+   </script>
