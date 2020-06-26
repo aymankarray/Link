89b8d0f106623b007063fd7a6c81354f349292e2
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-06-26 15:31:29 +0100

    Update resetPassword.php

--- application/views/email/resetPassword.php
+++ application/views/email/resetPassword.php
@@ -13,15 +13,17 @@
 
 <body>
 <div class="row" >
-      <div class="col-md-3" ></div>
-      <div class="col-md-6" >
+      <div class="col-md-4" ></div>
+      <div class="col-md-4" >
 <img src="https://www.tunivisions.link/images/logo%20rouge.svg" width="150px" ></img>
 <br>
 <h1 style="text-align:center">Bonjour , <?php echo $name ?></h1>
 <p>	
 Vous avez récemment demandé la réinitialisation de votre mot de passe. Il vous suffit de cliquer sur le bouton ci-dessous pour en définir un nouveau.</p>
 <br>
-<a href="<?php echo base_url() ?>Register/MotDePassechange/<?php echo $userId ?>" style="border-radius:50px;background-color:#d92829;display:inline-block;font-size:13px;border:none;margin:0px;font-family:Circular,&quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;text-align:left;text-decoration:none;padding:12px 45px!important;color:white!important;font-weight:bold!important" bgcolor="#1ED760" align="left" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://accounts.spotify.com/recover?username%3D315o47hzf3crhc6ilamtnhxmnto4%23NApPCjcKDVNwb3RpZnktdXNlcnMSHDMxNW80N2h6ZjNjcmhjNmlsYW10bmh4bW50bzQaA5gBASXUZEJcEhSNVyFcMyyWp0lcmtLfiUhcK8_sVQ&amp;source=gmail&amp;ust=1593264942468000&amp;usg=AFQjCNGlfiio-2vmgLuWajCNXxhF8R_S_w">DÉFINIR UN NOUVEAU MOT DE</span> <span class="il">PASSE</span></a>
+<center>
+<a href="<?php echo base_url() ?>Register/MotDePassechange/<?php echo $userId ?>" style="border-radius:50px;background-color:#d92829;display:inline-block;font-size:13px;border:none;margin:0px;font-family:Circular,&quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;text-align:left;text-decoration:none;padding:12px 45px!important;color:white!important;font-weight:bold!important" bgcolor="#1ED760" align="center" target="_blank" >DÉFINIR UN NOUVEAU MOT DE</span> <span class="il">PASSE</span></a>
+</center>
 <br><br>
 <p>	
 Si vous n'avez pas demandé la réinitialisation de votre mot de passe, vous pouvez ignorer cet e-mail.
@@ -30,7 +32,7 @@ Si vous n'avez pas demandé la réinitialisation de votre mot de passe, vous pou
 <p>L'équipe T-Link</p>
 
 </div>
-<div class="col-md-3" ></div>
+<div class="col-md-4" ></div>
 </body>
 
   <script    src="<?php echo base_url() ;  ?>assets/js/bootstrap.min.js"></script>
