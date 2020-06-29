70eb3023607cc7a099529dec0dfb3357496c5903
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-06-29 15:06:33 +0100

    Update users.php

--- application/views/users.php
+++ application/views/users.php
@@ -88,8 +88,8 @@
                         
                         <td>
                             <A HREF="mailto:<?php echo $record->email ?>"><i class="ri-mail-fill"></i></A> 
-                            <a href="tel:<?php echo $record->mobile ?>"><i class="ri-phone-fill"></i></i></a>
-                            <a href="<?php echo $record->facebook ?>"><i class="ri-facebook-box-fill"></i></i></i></a>
+                            <a href="tel:<?php echo $record->mobile ?>"><i class="ri-phone-fill"></i <?php echo $record->mobile ?></a>
+                            <a href="<?php echo $record->facebook ?>"><i class="ri-facebook-box-fill"></i></a>
                          </td>
 
                         <td>
