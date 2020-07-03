c022b4e0b790dd821d1050d0b3a2b9fa20f2241b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-07-03 21:45:30 +0100

    Update Register.php

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -66,27 +66,74 @@ class Register extends CI_Controller
                     $mail->isHTML(true);
                     
                     // Email body content
-                    $body = '<h1>Bonjour ,  '.$data['name'].' </h1> </br></br>' ;
-                    $body .= '<p>   
-                  Vous avez récemment demandé la réinitialisation de votre mot de passe. Il vous suffit de cliquer sur le bouton ci-dessous pour en définir un nouveau.</p>
-                  
+                    
+                  $body .='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-                        </br>
-                        <a href="'.base_url().'Register/Passechange/'.$data['userId'].'?userID='.$data['userId'].'" style="border-radius:50px;background-color:#d92829;display:inline-block;font-size:13px;border:none;margin:0px;font-family:Circular,&quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;text-align:left;text-decoration:none;padding:12px 45px!important;color:white!important;font-weight:bold!important" bgcolor="#1ED760" align="center" target="_blank" >
-                              DÉFINIR UN NOUVEAU MOT DE <span class="il">PASSE</span>
-                        </a>
-                        </br>
-                  
+                    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+                    <head>
+                                <!--[if gte mso 9]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]-->
+                                <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+                                <meta content="width=device-width" name="viewport"/>
+                                <!--[if !mso]><!-->
+                                <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+                          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                          <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/bootstrap.min.css">
+                          <!-- Typography CSS -->
+                          <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/typography.css">
+                          <!-- Style CSS -->
+                          <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/style.css">
+                          <!-- Responsive CSS -->
+                          <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/responsive.css">
+                          <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/dropify.min.css">
 
-                  
+                    </head>';
+                                        $body .= '<body>
+                          <div class="row" >
 
-                  <p>   
-                  Si vous n\'avez pas demandé la réinitialisation de votre mot de passe, vous pouvez ignorer cet e-mail.
-                  </p>
+                                <div class="col-md-4" >
+                                      
+                                </div>
+                                <div class="col-md-4"  >
+                                      <img src="https://www.tunivisions.link/images/logo%20rouge.svg" width="150px" ></img>
+                                      
+                                      <br>
+                                <h1>Bonjour ,  '.$data['name'].' </h1> </br></br>' ;
+                                        $body .= '<p>   
+                                      Vous avez récemment demandé la réinitialisation de votre mot de passe. Il vous suffit de cliquer sur le bouton ci-dessous pour en définir un nouveau.</p>
+                                      
 
-                  </br>
+                                            </br>
+                                            <a href="'.base_url().'Register/Passechange/'.$data['userId'].'?userID='.$data['userId'].'" style="border-radius:50px;background-color:#d92829;display:inline-block;font-size:13px;border:none;margin:0px;font-family:Circular,&quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;text-align:left;text-decoration:none;padding:12px 45px!important;color:white!important;font-weight:bold!important" bgcolor="#1ED760" align="center" target="_blank" >
+                                                  DÉFINIR UN NOUVEAU MOT DE <span class="il">PASSE</span>
+                                            </a>
+                                            </br>
+                                      
 
-                  <p>L\'équipe T-Link</p>' ;
+                                      
+
+                                      <p>   
+                                      Si vous n\'avez pas demandé la réinitialisation de votre mot de passe, vous pouvez ignorer cet e-mail.
+                                      </p>
+
+                                      </br>
+
+                                      <p>L\'équipe T-Link</p>
+
+
+                                      </div>
+
+                                <div class="col-md-4" >
+                                      
+                                </div>
+
+                          </div>
+                    </body>
+
+                      <script    src="https://www.tunivisions.link/assets/js/bootstrap.min.js"></script>
+                      
+                    </html>
+
+                    ' ;
 
                     $mail->Body =  $body  ; 
                     
