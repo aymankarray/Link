2956b950bb2682c9508396290ef294ba04c8b25f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-07-03 19:06:15 +0100

    Update resetPassword.php

--- application/views/email/resetPassword.php
+++ application/views/email/resetPassword.php
@@ -1,6 +1,7 @@
-<html lang="fr" >
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-      <meta charset="utf-8">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/bootstrap.min.css">
       <!-- Typography CSS -->
       <link rel="stylesheet" href="https://www.tunivisions.link/assets/css/typography.css">
