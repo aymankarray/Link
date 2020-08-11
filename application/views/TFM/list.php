0d1f6df8ea4655b8c6e5d78ae04afd1774879989
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-08-11 11:58:54 -0700

    Update list.php

--- application/views/TFM/list.php
+++ application/views/TFM/list.php
@@ -76,7 +76,7 @@
                                     <input type="submit" name="envoyer" class="btn btn-primary" >
                                 </form>
 
-                        <?php }  else if( (empty($tfmPart)) )  { ?>
+                        <?php }  else if(  $record->tfmId==7 )  { ?>
 
                                 <a class="btn btn-danger" href="<?php echo base_url() ?>TFM/partanTfm" >Je veux participer</a>
 
