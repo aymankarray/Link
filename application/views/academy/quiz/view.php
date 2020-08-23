6b23bdee46cc68f8ca763067cfeb1474147b021b
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-08-23 23:16:47 +0100

    Update view.php

--- application/views/academy/quiz/view.php
+++ application/views/academy/quiz/view.php
@@ -1,4 +1,30 @@
-
+<style type="text/css">
+    body{
+    font-size: 20px;
+    font-family: sans-serif;
+    color: #333;
+}
+.question{
+    font-weight: 600;
+}
+.answers {
+    margin-bottom: 20px;
+}
+#submit{
+    font-family: sans-serif;
+    font-size: 20px;
+    background-color: #297;
+    color: #fff;
+    border: 0px;
+    border-radius: 3px;
+    padding: 20px;
+    cursor: pointer;
+    margin-bottom: 20px;
+}
+#submit:hover{
+    background-color: #3a8;
+}
+</style>
 
  <div id="content-page" class="content-page" onload="countDown()" >
     <div class="container" >
