7074ed0f38fe1e06887ebc851df3a7b73ee3d263
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-08-23 22:58:18 +0100

    Update view.php

--- application/views/academy/quiz/view.php
+++ application/views/academy/quiz/view.php
@@ -35,7 +35,7 @@
 
 <script type="text/javascript">
     // Set the date we're counting down to
-                                var countDownDate = new Date().getTime() + 15 * 60000;
+                                var countDownDate = new Date().getTime() + 2 * 60000;
 
                                 // Update the count down every 1 second
                                 var x = setInterval(function() {
