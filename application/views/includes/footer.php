6ccc766e1e113af1a429a0d9720a19cfbdbcbfae
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-08-23 22:54:16 +0100

    quiz

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -26,7 +26,7 @@
       <script   src="<?php echo base_url() ;  ?>assets/js/popper.min.js"></script>
       <script    src="<?php echo base_url() ;  ?>assets/js/bootstrap.min.js"></script>
       <!-- Appear JavaScript -->
-      <script   src="<?php echo base_url() ;  ?>assets/js/jquery.appear.js"></script>
+      <script defer  src="<?php echo base_url() ;  ?>assets/js/jquery.appear.js"></script>
       <!-- Countdown JavaScript -->
       <script  src="<?php echo base_url() ;  ?>assets/js/countdown.min.js"></script>
       <!-- Counterup JavaScript -->
@@ -290,6 +290,124 @@
 
         })(jQuery);
          </script>
+
+
+         <script type="text/javascript">
+
+var myQuestions = [
+    <?php foreach ($questions as $key ) { ?>
+    {
+        question: "<?php echo $key->Question ; ?> ",
+        answers: {
+            a: '<?php echo $key->choice1 ; ?>',
+            b: '<?php echo $key->choice2 ; ?>',
+            c: '<?php echo $key->choice3 ; ?>',
+            d: '<?php echo $key->choice4 ; ?>'
+        },
+        correctAnswer: '<?php echo $key->Answer1 ; ?>'
+    },
+    <?php } ?> 
+    
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'"> &nbsp;&nbsp; '
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label> <br>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question"> <h4>' + questions[i].question + '</h4></div> <br> '
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        var a = (numCorrect / (questions.length + numCorrect)) * 100
+        var b = 100 - a
+        if( b>70  ){   alert('Félicitaion vous avez réussir dans ce exam')  ; }
+        
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + '/' + questions.length + ' = ' b;
+
+       
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+        quizContainer.style.display = 'none';
+        submitButton.style.display = 'none';
+    }
+
+}
+
+</script>
+
  
    
 <svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;">
