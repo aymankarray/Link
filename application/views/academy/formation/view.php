fac0d21dc464c9c847e46b3efdcbd58c444f125f
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-08-24 20:42:00 +0100

    Update view.php

--- application/views/academy/formation/view.php
+++ application/views/academy/formation/view.php
@@ -209,7 +209,7 @@
 
 
                                    <input type="checkbox" name="" required="">&nbsp; j'accepte ce reglement de certification<br>
-                                   <input type="submit"  class="btn btn-primary" value="Envoyer" disabled="true" >
+                                   <input type="submit"  class="btn btn-primary" value="Envoyer" <?php if ($SA == 0 ){ ?> disabled="true"<?php } ?> >
                                    </form> 
                                  </div>
                                  
