8f00e6ac7adb13be5fca3b5a1cbd24282e2cb3b3
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-09-22 13:20:41 -0700

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -244,10 +244,10 @@
                               <?php foreach ($ActuRecords as $key ) { ?>
                               <li class="d-flex align-items-center">
                                 <a href="<?php echo base_url() ?>Actu/Show/<?php echo $key->actuID ?>">
-                                 <img src="<?php echo base_url() ?>Uploads/Actu/<?php echo $key->image ?>" alt="" class="img-fluid">
+                                 <img src="<?php echo base_url() ?>Uploads/Actu/<?php echo $key->image ?>" class="img-fluid" >
                                  </a>
                                  <div class="stories-data ml-3">
-                                    <h5><?php $key->titre ?></h5>
+                                    <h5><?php echo $key->titre ?></h5>
                                     <p class="mb-0">Il y a <?php echo xTimeAgo($key->createdBy,date('Y-m-d H:i:s')) ; ?></p>
                                  </div>
 
