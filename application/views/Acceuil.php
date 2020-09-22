d97258d53fff755fb4304d27b2c44d2071d1c9ff
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-09-22 12:55:18 -0700

    Update Acceuil.php

--- application/views/Acceuil.php
+++ application/views/Acceuil.php
@@ -77,9 +77,11 @@
                      </div> 
 
                      <!-- pub -->
+                     <div class="col-sm-12">
                      <a href="https://www.facebook.com/ChahiaOfficielle" target="_blank" >
                      <img src="https://live.tunivisions.net/adserver/tunivisions/www/images/2e7aacdf30624a2e3b8c84f2f5ac6d72.jpg"  class="img-fluid rounded w-100" >
                       </a>
+                    </div>
                      <!-- end pub-->
                     <!-- publication  --> 
                     <?php foreach ($postRecords as $post ) { ?> 
