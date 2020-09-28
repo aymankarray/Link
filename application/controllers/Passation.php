be7d8227af71ca03a5cef2be6a1a864ce4711a57
maiza BahaEdinne <36070638+MaizaBahaedinne@users.noreply.github.com>
2020-09-28 09:34:05 -0700

    Update Passation.php

--- application/controllers/Passation.php
+++ application/controllers/Passation.php
@@ -244,7 +244,7 @@ public function AccepteDossier ()
              foreach ( $ids as $key ) {
 
               $Dossier =   $this->passation_model->PassationById($key) ;
-            echo  $Dossier->clubID .' '.$Dossier->celluleVol.' '.$Dossier->roleId.''.$Dossier->name   ;
+            echo  $Dossier->clubID .'c '.$Dossier->celluleVol.' c'.$Dossier->roleId.' r '.$Dossier->name   ;
               $CandidatActuel = $this->user_model->getMemberByRoleAndCelulle($Dossier->clubID,'',$Dossier->roleId);
              echo  $CandidatActuel->name   ;
 
