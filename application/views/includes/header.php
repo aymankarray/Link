d299ffc0fc979df664760e677edd6b61735e1c99
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-01 23:23:22 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,3 +1,16 @@
+<?php 
+                     function xTimeAgo ($oldTime, $newTime) {
+                      $timeCalc = strtotime($newTime) - strtotime($oldTime);
+                      if ($timeCalc > (60*60*24)) {$timeCalc = round($timeCalc/60/60/24) . " jours";}
+                      else if ($timeCalc > (60*60)) {$timeCalc = round($timeCalc/60/60) . " heures";}
+                      else if ($timeCalc > 60) {$timeCalc = round($timeCalc/60) . " minutes";}
+                      else if ($timeCalc > 0) {$timeCalc .= " seconds";}
+                      return $timeCalc;
+                      }
+                      ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
