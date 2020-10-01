3683915f69f21a8cddbd4fc788f08771376dd6fb
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-01 19:22:34 +0100

    Update BaseController.php

--- application/libraries/BaseController.php
+++ application/libraries/BaseController.php
@@ -177,10 +177,13 @@ class BaseController extends CI_Controller {
 				                  
 
          $headerInfo['ConnrectedUser'] =  $this->login_model->lastLogins() ;
-        
+        if($this->SA != 1){
         $this->load->view('includes/header', $headerInfo);
         $this->load->view($viewName, $pageInfo);
         $this->load->view('includes/footer', $footerInfo);
+        }else {
+			$this->load->view ( 'soon' );
+		}
     }
 	
 	/**
