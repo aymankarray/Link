9fad5718054d62ad30b1ba7a8ea69b717d3343e4
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-01 23:07:33 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -32,6 +32,19 @@
 </head>
 
 <body>
+	<div class="wavy-wraper">
+		<div class="wavy">
+		  <span style="--i:1;">p</span>
+		  <span style="--i:2;">i</span>
+		  <span style="--i:3;">t</span>
+		  <span style="--i:4;">n</span>
+		  <span style="--i:5;">i</span>
+		  <span style="--i:6;">k</span>
+		  <span style="--i:7;">.</span>
+		  <span style="--i:8;">.</span>
+		  <span style="--i:9;">.</span>
+		</div>
+	</div>
 	<nav id="shoppingbag" class="mm-menu mm-offcanvas mm-right" aria-hidden="true">
 	<div class="mm-panels"><div class="mm-panel mm-hasnavbar mm-opened" id="mm-13">
 		<div class="mm-navbar">
