dafad11b53df11ec9986b7097ef1cffd564158ed
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-01 23:05:39 +0100

    Update header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -249,7 +249,7 @@
 				<a class="" href="#menu"><i class="fa fa-align-justify"></i></a>
 			</span>
 			<span class="mh-text">
-				<a href="<?php echo base_url()?>" title=""><img src="<?php echo base_url()?>assets/images/logo2.svg"  width="45%" alt=""></a>
+				<a href="<?php echo base_url()?>" title=""><img src="<?php echo base_url()?>assets/images/logo.svg"  width="45%" alt=""></a>
 			</span>
 			<span class="mh-btns-right">
 				<a class="fa fa-sliders" href="#shoppingbag"></a>
