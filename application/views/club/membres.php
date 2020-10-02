e113d8e4dc094fc9688f8c34170407712689cbcd
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-02 19:49:43 +0100

    Update membres.php

--- application/views/club/membres.php
+++ application/views/club/membres.php
@@ -5,6 +5,96 @@
           <div class="col-lg-12">
             <div class="row merged20" id="page-contents">
               <!-- user profile banner  -->
+                           <div class="user-profile">
+                <figure>
+                  <div class="edit-pp">
+                    <label class="fileContainer">
+                      <i class="fa fa-camera"></i>
+                      <input type="file">
+                    </label>
+                  </div>
+                  <img src="<?php echo base_url()?>assets/images/resources/fav-banner.jpg" alt="">                  
+                  <ul class="profile-controls">
+                    <li><a href="#" title="" data-toggle="tooltip" data-original-title="Like"><i class="fa fa-thumbs-up"></i></a></li>
+                    <li><a href="#" title="" data-toggle="tooltip" data-original-title="Follow"><i class="fa fa-star"></i></a></li>
+                    <li><a class="send-mesg" href="#" title="" data-toggle="tooltip" data-original-title="Send Message"><i class="fa fa-comment"></i></a></li>
+                    <li>
+                      <div class="edit-seting" title="" data-toggle="tooltip" data-original-title="Edit Profile image"><i class="fa fa-sliders"></i>
+                        <ul class="more-dropdown">
+                          <li>
+                            <a href="#" title="">Share this Page</a>
+                          </li>
+                          <li>
+                            <a href="#" title="">Update Profile Photo</a>
+                          </li>
+                          <li>
+                            <a href="#">Update Header Photo</a>
+                          </li>
+                          <li>
+                            <a href="setting.html" title="" data-toggle="tooltip" data-original-title="">Account Settings</a>
+                          </li>
+                          <li>
+                            <a href="timeline-friends.html" title="">Invite Friends</a>
+                          </li>
+                          <li>
+                            <a href="support-and-help.html" title="">Find support</a>
+                          </li>
+                          <li>
+                            <a href="setting.html" title="">Block Page</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                  </ul>
+                  <ol class="pit-rate">
+                    <li class="rated"><i class="fa fa-star"></i></li>
+                    <li class="rated"><i class="fa fa-star"></i></li>
+                    <li class="rated"><i class="fa fa-star"></i></li>
+                    <li class="rated"><i class="fa fa-star"></i></li>
+                    <li class=""><i class="fa fa-star"></i></li>
+                    <li><span>4.7/5</span></li>
+                  </ol>
+                </figure>
+                
+                <div class="profile-section">
+                  <div class="row">
+                    <div class="col-lg-2">
+                      <div class="profile-author">
+                        <a class="profile-author-thumb" href="about.html">
+                          <img alt="author" src="images/resources/fav-dp.jpg">
+                          <div class="edit-dp">
+                            <label class="fileContainer">
+                              <i class="fa fa-camera"></i>
+                              <input type="file">
+                            </label>
+                          </div>
+                        </a>
+                        <div class="author-content">
+                          <a class="h4 author-name" href="#"><?php echo $clubInfo->name ?></a>
+                          <div class="country"><?php echo $clubInfo->city ?></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-10">
+                      <ul class="profile-menu">
+                        <li>
+                          <a class="active" href="<?php echo base_url() ?>Club/clubInfo/<?php echo $cl ?>">Projets</a>
+                        </li>
+                        <li>
+                          <a class="" href="<?php echo base_url() ?>Club/clubInfo/<?php echo $cl ?>">Membes</a>
+                        </li>
+
+                        
+                      </ul>
+                      <ol class="folw-detail">
+                        <li><span>Posts</span><ins>101</ins></li>
+                        <li><span>Followers</span><ins>1.3K</ins></li>
+                        <li><span>Members</span><ins>22k</ins></li>
+                      </ol>
+                    </div>
+                  </div>
+                </div>  
+              </div>
               <div class="col-lg-12">
                 <div class="central-meta">
                   <div class="title-block">
