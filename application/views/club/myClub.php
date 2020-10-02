9bb3564392212c3e4f71589efa2cae32bf7bd334
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-02 01:50:47 +0100

    Update myClub.php

--- application/views/club/myClub.php
+++ application/views/club/myClub.php
@@ -1123,6 +1123,19 @@
               <div class="col-lg-3">
                 <aside class="sidebar static right">
                   <div class="widget">
+                    <h4 class="widget-title">Mon club</h4>
+                    <ul class="fav-community">
+                      <li><i class="fa fa-address-card"></i> About <p>We are motel hotel from Los Angeles, now based in San Francisco, come and enjoy!</p></li>
+                      <li><i class="fa fa-users"></i><a href="#" title="">invite friends</a> to like this page</li>
+                      <li><i class="fa fa-thumbs-up"></i>13,33,454 People like this</li>
+                      <li><i class="fa fa-rss"></i>13,33,454 People follow this</li>
+                      <li><i class="fa fa-share-alt"></i>13,540 People share this</li>
+                      <li><i class="fa fa-bookmark"></i><a href="#" title="">category</a> Entertainment</li>
+                      <li><i class="fa fa-globe"></i><a href="<?php echo $clubInfo->email ?>" title="">club Tunivisions <?php echo $clubInfo->facebook ?></a></li>
+                      <li><i class="fa fa-map-marker"></i><?php echo $clubInfo->facebook ?></li>
+                    </ul>
+                  </div>
+                  <div class="widget">
                     <h4 class="widget-title">Share Page</h4>
                     <ul class="socials">
                       <li class="facebook">
