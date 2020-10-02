a4a73b8578a75a3e960429bbf21db31562124058
Maiza Bahaedinne <bahaedinne.maiza@esprit.tn>
2020-10-02 14:20:57 +0100

    Update list.php

--- application/views/project/list.php
+++ application/views/project/list.php
@@ -93,6 +93,247 @@
                                         </div>
                                     </div>
                             </div><!-- centerl meta -->
+                            <div class="col-lg-3">
+                                <aside class="sidebar static right">
+                                    <div class="widget">
+                                        <h4 class="widget-title">Complete Your Profile</h4>
+                                        <div class="pit-reg-complete">
+                                            <div class="progresdiv" data-percent="86">
+                                                  <svg class="progres" width="90" height="90" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                                  <circle r="50" cx="50" cy="50" fill="transparent" stroke-dasharray="502.4" stroke-dashoffset="0"></circle>
+                                                  <circle class="bar" r="50" cx="50" cy="50" fill="transparent" stroke-dasharray="502.4" stroke-dashoffset="0" style="stroke-dashoffset: 432.064;"></circle>
+                                                </svg>
+                                            </div>
+                                            <div class="reg-comp-meta">
+                                                <p>filling your profile details will help you to meet the right people</p>
+                                                <ul>
+                                                    <li><i class="fa fa-envelope-o bg-red"></i> <span>Profile Information 20%</span> 
+                                                        <a href="#" title="" class="underline">Add</a>
+                                                    </li>
+                                                    <li><i class="fa fa-envelope-o bg-blue"></i> <span>your Email 10%</span> 
+                                                        <a href="#" title="" class="underline">Add</a>
+                                                    </li>
+                                                    <li><i class="fa fa-phone bg-purple"></i> <span>your phone number 10%</span> 
+                                                        <a href="#" title="" class="underline">Add</a>
+                                                    </li>
+                                                    <li><i class="fa fa-map-marker bg-green"></i> <span>Your Location 10%</span>
+                                                        <a href="#" title="" class="underline">Add</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div><!-- complete profile widget -->
+                                    <div class="widget">
+                                        <h4 class="widget-title">Your page</h4> 
+                                        <div class="your-page">
+                                            <figure>
+                                                <a href="#" title=""><img src="images/resources/friend-avatar9.jpg" alt=""></a>
+                                            </figure>
+                                            <div class="page-meta">
+                                                <a href="#" title="" class="underline">My Creative Page</a>
+                                                <span><i class="ti-comment"></i><a href="insight.html" title="">Messages <em class="bg-blue">9</em></a></span>
+                                                <span><i class="ti-bell"></i><a href="insight.html" title="">Notifications <em class="bg-purple">2</em></a></span>
+                                            </div>
+                                            <ul class="page-publishes">
+                                                <li>
+                                                    <span><i class="ti-pencil-alt"></i>Publish</span>
+                                                </li>
+                                                <li>
+                                                    <span><i class="ti-camera"></i>Photo</span>
+                                                </li>
+                                                <li>
+                                                    <span><i class="ti-video-camera"></i>Live</span>
+                                                </li>
+                                                <li>
+                                                    <span><i class="fa fa-user-plus"></i>Invite</span>
+                                                </li>
+                                            </ul>
+                                            <div class="page-likes">
+                                                <ul class="nav nav-tabs likes-btn">
+                                                    <li class="nav-item"><a class="active" href="#link1" data-toggle="tab" data-ripple="">likes</a></li>
+                                                     <li class="nav-item"><a class="" href="#link2" data-toggle="tab" data-ripple="">views</a></li>
+                                                </ul>
+                                                <!-- Tab panes -->
+                                                <div class="tab-content">
+                                                  <div class="tab-pane active fade show" id="link1">
+                                                    <span><i class="ti-heart"></i>884</span>
+                                                      <a href="#" title="weekly-likes">35 new likes this week</a>
+                                                      <div class="users-thumb-list">
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Anderw">
+                                                            <img src="images/resources/userlist-1.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="frank">
+                                                            <img src="images/resources/userlist-2.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Sara">
+                                                            <img src="images/resources/userlist-3.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Amy">
+                                                            <img src="images/resources/userlist-4.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Ema">
+                                                            <img src="images/resources/userlist-5.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Sophie">
+                                                            <img src="images/resources/userlist-6.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Maria">
+                                                            <img src="images/resources/userlist-7.jpg" alt="">  
+                                                        </a>  
+                                                      </div>
+                                                  </div>
+                                                  <div class="tab-pane fade" id="link2">
+                                                      <span><i class="fa fa-eye"></i>440</span>
+                                                      <a href="#" title="weekly-likes">440 new views this week</a>
+                                                      <div class="users-thumb-list">
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Anderw">
+                                                            <img src="images/resources/userlist-1.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="frank">
+                                                            <img src="images/resources/userlist-2.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Sara">
+                                                            <img src="images/resources/userlist-3.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Amy">
+                                                            <img src="images/resources/userlist-4.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Ema">
+                                                            <img src="images/resources/userlist-5.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Sophie">
+                                                            <img src="images/resources/userlist-6.jpg" alt="">  
+                                                        </a>
+                                                        <a href="#" title="" data-toggle="tooltip" data-original-title="Maria">
+                                                            <img src="images/resources/userlist-7.jpg" alt="">  
+                                                        </a>  
+                                                      </div>
+                                                  </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><!-- page like widget -->
+                                    <div class="widget">
+                                        <div class="weather-widget low-opacity bluesh">
+                                            <div class="bg-image" style="background-image: url(images/resources/weather.jpg)"></div>
+                                            <span class="refresh-content"><i class="fa fa-refresh"></i></span>
+                                            <div class="weather-week">
+                                                <div class="icon sun-shower">
+                                                  <div class="cloud"></div>
+                                                  <div class="sun">
+                                                    <div class="rays"></div>
+                                                  </div>
+                                                  <div class="rain"></div>
+                                                </div>
+                                            </div>
+                                            <div class="weather-infos">
+                                                <span class="weather-tem">25</span>
+                                                <h3>Cloudy Skyes<i>Sicklervilte, New Jersey</i></h3>
+                                                <div class="weather-date skyblue-bg">
+                                                    <span>MAY<strong>21</strong></span>
+                                                </div>
+                                            </div>
+                                            <div class="monthly-weather">
+                                                <ul>
+                                                    <li>
+                                                        <span>Sun</span>
+                                                        <a href="#" title=""><i class="wi wi-day-sunny"></i></a>
+                                                        <em>40°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Mon</span>
+                                                        <a href="#" title=""><i class="wi wi-day-cloudy"></i></a>
+                                                        <em>10°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Tue</span>
+                                                        <a href="#" title=""><i class="wi wi-day-hail"></i></a>
+                                                        <em>20°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Wed</span>
+                                                        <a href="#" title=""><i class="wi wi-day-lightning"></i></a>
+                                                        <em>34°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Thu</span>
+                                                        <a href="#" title=""><i class="wi wi-day-showers"></i></a>
+                                                        <em>22°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Fri</span>
+                                                        <a href="#" title=""><i class="wi wi-day-windy"></i></a>
+                                                        <em>26°</em>
+                                                    </li>
+                                                    <li>
+                                                        <span>Sat</span>
+                                                        <a href="#" title=""><i class="wi wi-day-sunny-overcast"></i></a>
+                                                        <em>30°</em>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            
+                                        </div><!-- Weather Widget -->
+                                    </div><!-- weather-->
+                                    <div class="widget">
+                                        <h4 class="widget-title">Invite Friends <a class="see-all" href="#" title="">See All</a></h4>
+                                        <ul class="invitepage">
+                                            <li>
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar.jpg" alt="">
+                                                    <a href="#">Jack carter</a>
+                                                </figure>
+                                                <a class="invited" href="#" title=""><i class="ti-check"></i></a>
+                                            </li>
+                                            <li>
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar2.jpg" alt="">
+                                                    <a href="#">Emma watson</a>
+                                                </figure>
+                                                <a href="#" title="">Invite</a>
+                                            </li>
+                                            <li>
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar3.jpg" alt="">
+                                                    <a href="#">Andrew</a>
+                                                </figure>
+                                                <a href="#" title="">Invite</a>
+                                            </li>
+                                            <li>
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar4.jpg" alt="">
+                                                    <a href="#">Moona Singh</a>
+                                                </figure>
+                                                <a class="invited" href="#" title=""><i class="ti-check"></i></a>
+                                            </li>
+                                            <li>
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar5.jpg" alt="">
+                                                    <a href="#">Harry pooter</a>
+                                                </figure>
+                                                <a href="#" title="">Invite</a>
+                                            </li>
+                                        </ul>
+                                        
+                                    </div><!-- page invitation widget -->   
+                                    <div class="widget stick-widget">
+                                        <div class="banner medium-opacity purple">
+                                            <div class="bg-image" style="background-image: url(images/resources/baner-widgetbg.jpg)"></div>
+                                            <div class="baner-top">
+                                                <span><img alt="" src="images/book-icon.png"></span>
+                                                <i class="fa fa-ellipsis-h"></i>
+                                            </div>
+                                            <div class="banermeta">
+                                                <p>
+                                                    create your own favourit page.
+                                                </p>
+                                                <span>like them all</span>
+                                                <a data-ripple="" title="" href="#">start now!</a>
+                                            </div>
+                                        </div>                                          
+                                    </div>
+                                </aside>
+                            </div>
                         </div>  
                     </div>
                 </div>
